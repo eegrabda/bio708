20a7edec8efbdcf867ccf0a15ccdd07eabaa5c4e
eegrabda <elisegrabda@gmail.com>
2023-10-05 09:14:16 -0400

    whoops

--- code/week_05.R
+++ code/week_05.R
@@ -4,8 +4,10 @@
 #' date: "`r format(Sys.time(), '%d-%m-%Y')`"
 #' author: Elise Grabda
 #' ---
-
-
+#+ message = F, warning = F
+library(tidyverse)
+library(patchwork)
+setwd("C:/Git/bio708")
 # Garden Plants -----------------------------------------------------------
 
 h <- c(16.9, 20.9, 15.8, 28, 21.6, 15.9, 22.4, 23.7, 22.9, 18.5)
@@ -34,9 +36,8 @@ df_h2 <- tibble(plant_id = 11:20, # a vector from 11 to 20 by 1
 print(df_h2)
 
 
-dfh0 <- read_csv("data_raw/data_plant_height.csv")
+df_h0 <- read_csv("data_raw/data_plant_height.csv")
 
-print(dfh0)
 
 mu <- mean(df_h0$height)
 sigma2 <- sum((df_h0$height - mu)^2) / nrow(df_h0)
@@ -145,3 +146,101 @@ g_var_ub <- df_sample %>%
 
 g_mu / g_var / g_var_ub
 g_mu | g_var / g_var_ub
+
+
+
+# Laboratory --------------------------------------------------------------
+mu_j <- NULL
+mu_k <- NULL
+var_j <- NULL
+var_k <- NULL
+
+for (i in 1:100) {
+  
+  df_j <- df_h0 %>% 
+    sample_n(size = 50) # random samples of 10 individuals
+  df_k <- df_h0 %>% 
+    sample_n(size = 100)
+  # save mean for sample set i
+  mu_j[i] <- mean(df_j$height)
+  mu_k[i] <- mean(df_k$height)
+  # save variance for sample set i
+  var_j[i] <- var(df_j$height) 
+  var_k[i] <- var(df_k$height)
+  
+}
+
+df_s <- tibble(mu_j,mu_k,var_j,var_k)
+
+muj <-  df_s %>% 
+  ggplot(aes(x = mu_j)) +
+  geom_histogram(color="Black") +
+  geom_vline(xintercept = mu,color="Green")
+
+muk <- df_s %>% 
+  ggplot(aes(x = mu_k)) +
+  geom_histogram(color = "Black") +
+  geom_vline(xintercept = mu,color="Green")
+
+varj <- df_s %>% 
+  ggplot(aes(x = var_j)) +
+  geom_histogram(color = "Black") +
+  geom_vline(xintercept = mu,color="Green")
+
+vark <- df_s %>% 
+  ggplot(aes(x = var_k)) +
+  geom_histogram(color = "Black") +
+  geom_vline(xintercept = sigma2,color="Green")
+
+
+muj/muk|varj/vark
+
+#Part 2
+
+df_h10 <- df_h0 %>% 
+  filter(height >= 10)
+
+mu_j1 <- NULL
+mu_k1 <- NULL
+var_j1 <- NULL
+var_k1 <- NULL
+
+for (i in 1:100) {
+  
+  df_j1 <- df_h10 %>% 
+    sample_n(size = 50) # random samples of 50 individuals
+  df_k1 <- df_h10 %>% 
+    sample_n(size = 100)
+  # save mean for sample set i
+  mu_j1[i] <- mean(df_j1$height)
+  mu_k1[i] <- mean(df_k1$height)
+  # save variance for sample set i
+  var_j1[i] <- var(df_j1$height) 
+  var_k1[i] <- var(df_k1$height)
+  
+}
+
+df_s1 <- tibble(mu_j1,mu_k1,var_j1,var_k1)
+
+muj1 <-  df_s1 %>% 
+  ggplot(aes(x = mu_j1)) +
+  geom_histogram(color="Black") +
+  geom_vline(xintercept = mu,color="Green")
+
+muk1 <- df_s1 %>% 
+  ggplot(aes(x = mu_k1)) +
+  geom_histogram(color = "Black") +
+  geom_vline(xintercept = mu,color="Green")
+
+varj1 <- df_s1 %>% 
+  ggplot(aes(x = var_j1)) +
+  geom_histogram(color = "Black") +
+  geom_vline(xintercept = mu,color="Green")
+
+vark1 <- df_s1 %>% 
+  ggplot(aes(x = var_k1)) +
+  geom_histogram(color = "Black") +
+  geom_vline(xintercept = sigma2,color="Green")
+
+
+muj1/muk1|varj1/vark1
---
+++ code/week_08.R
@@ -0,0 +1,119 @@
+#' ---
+#' title: Week Eight Code
+#' output: html_document
+#' date: "`r format(Sys.time(), '%d-%m-%Y')`"
+#' author: Elise Grabda
+#' ---
+#+ message = F, warning = F
+library(tidyverse)
+library(here)
+
+
+
+# Multiple Groups ---------------------------------------------------------
+
+df_anova <- read_csv(here("data_raw/data_fish_length_anova.csv"))
+distinct(df_anova, lake)
+
+# geom_violin() - function for violin plots
+# geom_jitter() - jittered points
+
+df_anova %>% 
+  ggplot(aes(x = lake,
+             y = length)) +
+  geom_violin(draw_quantiles = 0.5, # draw median horizontal line
+              alpha = 0.2) + # transparency
+  geom_jitter(alpha = 0.2) # transparency
+
+
+
+# ANOVA -------------------------------------------------------------------
+
+# first argument is formula
+# second argument is data frame for reference
+# do not forget specify data = XXX! aov() refer to columns in the data frame
+m <- aov(formula = length ~ lake,
+         data = df_anova)
+
+print(m)
+summary(m)
+
+
+
+# Behind Scenes -----------------------------------------------------------
+# estimate overall mean
+mu <- mean(df_anova$length)
+
+# estimate group means and sample size each
+df_g <- df_anova %>% 
+  group_by(lake) %>% 
+  summarize(mu_g = mean(length), # mean for each group
+            dev_g = (mu_g - mu)^2, # squared deviation for each group
+            n = n()) # sample size for each group
+
+print(df_g)
+
+df_g <- df_g %>% 
+  mutate(ss = dev_g * n)
+
+print(df_g)
+
+s_b <- sum(df_g$ss)
+print(s_b)
+
+df_i <- df_anova %>% 
+  group_by(lake) %>% 
+  mutate(mu_g = mean(length)) %>% # use mutate() to retain individual rows
+  ungroup() %>% 
+  mutate(dev_i = (length - mu_g)^2) # deviation from group mean for each fish
+
+# filter() & slice(): show first 3 rows each group
+print(df_i %>% filter(lake == "a") %>% slice(1:3))
+
+print(df_i %>% filter(lake == "b") %>% slice(1:3))
+
+print(df_i %>% filter(lake == "c") %>% slice(1:3))
+
+
+df_i_g <- df_i %>% 
+  group_by(lake) %>% 
+  summarize(ss = sum(dev_i))
+
+print(df_i_g)
+s_w <- sum(df_i_g$ss)
+print(s_w)
+
+
+# n_distinct() count the number of unique elements
+n_g <- n_distinct(df_anova$lake)
+s2_b <- s_b / (n_g - 1)
+print(s2_b)
+
+s2_w <- s_w / (nrow(df_anova) - n_g)
+print(s2_w)
+
+
+
+# Laboratory --------------------------------------------------------------
+
+PG <- PlantGrowth
+
+PGroup <- PG %>% 
+  group_by(group) %>% 
+  summarise(mu = mean(weight),
+            sd = sd(weight))
+
+
+PG %>% 
+  ggplot(aes(x=group, y=weight))+
+  geom_violin(fill="steelblue",alpha=0.5)+
+  geom_jitter(width=0.3,
+              height=0)+
+  geom_segment(data=PGroup,
+               aes(x=group,
+                   xend=group,
+                   y=mu-sd,
+                   yend=mu+sd))
+
+ANOVA_PG <- aov(weight ~ group, data = PG)
+summary(ANOVA_PG)
